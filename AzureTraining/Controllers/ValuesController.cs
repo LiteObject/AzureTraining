badae3aa0734464aef81355bdb3d9b3f90fb8c33
Mohammed <tuniis@live.com>
2020-06-01 22:21:30 -0500

    Add project files.

---
+++ AzureTraining/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureTraining.Controllers
+{
+    [Route("/")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            List<string> values = await this.ValueRepo(default);
+            return this.Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id == default)
+            {
+                return BadRequest($"Invalid id: {id}");
+            }
+
+            List<string> values = await this.ValueRepo(id);
+            return this.Ok(values.FirstOrDefault());
+        }
+
+        /// <summary>
+        /// The value repo.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        private Task<List<string>> ValueRepo(int id)
+        {
+            var values = new List<string>();
+
+            if (id == default)
+            {
+                for (var i = 0; i < 1000; i++)
+                {
+                    values.Add($"value{i}");
+                }
+            }
+            else
+            {
+                values.Add($"value{id}");
+            }
+
+            return Task.FromResult(values);
+        }
+    }
+}
